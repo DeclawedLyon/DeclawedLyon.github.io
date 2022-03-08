63086bc504d9d6f70149f79f3516a10f32f1aabb
DeclawedLyon <declan.e.lyons@gmail.com>
2022-03-07 17:54:03 -0700

    adds basic css styling. Starts fleshing out color pallet variables

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFire } from '@fortawesome/free-solid-svg-icons';
+// import { fab } from '@fortawesome/free-brands-svg-icons'
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 // consider moving styles to css style page
 
@@ -13,7 +14,7 @@ function App() {
       <div id="about-me-field">
         <img class='profile-picture' src='https://media-exp1.licdn.com/dms/image/C5603AQGBQ9umKsqvrw/profile-displayphoto-shrink_800_800/0/1634181442830?e=1652313600&v=beta&t=6JRYUmH2ZypD9du4ubhjhN9CRsiwAvR9cnuNQmPsfOQ' id='profile-picture' alt='profile picture' width={300} height={300}></img>
         <p><FontAwesomeIcon icon={faFire} /> Description about myself. How I got into coding and where I'd like to take it</p>
-        {/* <FontAwesomeIcon icon={brands('github')} /> */}
+        <FontAwesomeIcon icon={solid('fire')} />
       </div>
 
       <div id="experience-field">
@@ -21,7 +22,7 @@ function App() {
       </div>
 
       <div id='project-field'>
-        link to projects. Include: Bridge. Tweeter(?). (want to build app for Devon. link to that(?))
+        link to projects. Include: Bridge. Tweeter(?). (want to build app for Elevation. link to that(?))
         <br></br>
         <a href='#'>Bridge</a>
       </div>
@@ -31,7 +32,7 @@ function App() {
         contact and phone information.
         Location / working hours / contact hours.
         <br></br>
-        <a href='#'>Link to my current resume</a>
+        {/* <a href='#'>Link to my current resume</a> */}
       </div>
 
       <footer style={{
@@ -43,9 +44,9 @@ function App() {
         // justifyContent: 'space-between',
         // 'flexDirection': 'column'
       }}>
-        <a href='#'>github</a>
-        <a href='#'>linkedIn</a>
-        <a href='#'>linkedIn</a>
+        <a href='#'>github<FontAwesomeIcon icon={brands('github-square')} /></a>
+        <a href='#'>linkedIn<FontAwesomeIcon icon={brands('linkedin')} /></a>
+        <a href='#'>resume<FontAwesomeIcon icon={solid('file-download')} /></a>
       </footer>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 export default function NavBar() {
 
   return (
-    <nav style={{
-      display: 'flex',
-      justifyContent: 'space-between'
-    }}>
+    <nav id="nav-bar">
       {/* These links will either be links to pages OR links to page locations */}
-      <a href="#">Logo</a>
-      <a href="#">About</a>
-      <a href="#">Experience</a>
-      <a href="#">Projects</a>
-      <a href="#">Contact</a>
-      <a href="#">Resume</a>
+      <a href="#" className="nav-item">Logo</a>
+      <a href="#" className="nav-item">About</a>
+      <a href="#" className="nav-item">Experience</a>
+      <a href="#" className="nav-item">Projects</a>
+      <a href="#" className="nav-item">Contact</a>
+      {/* <a href="#">Resume</a> */}
     </nav>
   )
 }
