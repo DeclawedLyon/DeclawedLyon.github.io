c20607546b633f05315785db5d57369a61f14fe0
DeclawedLyon <declan.e.lyons@gmail.com>
2022-04-18 22:53:25 -0700

    creates loading page component

--- src/components/backyard/Backyard.js
+++ src/components/backyard/Backyard.js
@@ -3,12 +3,14 @@ import './back-yard.css'
 import Radio from './Radio'
 import BookSearch from './textSearch/BookSearch'
 import Logo from './logo/Logo'
+import SiteLoading from './page_load/SiteLoading'
 
 export default function Backyard() {
   return (
-    <>
+    <div id="backyard">
+      {/* <SiteLoading /> */}
       <BookSearch />
       {/* <Logo /> */}
-    </>
+    </div>
   )
 }
