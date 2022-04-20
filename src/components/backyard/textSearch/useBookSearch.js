42f862daa03ed20f980f623b492cdb72078b195f
DeclawedLyon <declan.e.lyons@gmail.com>
2022-04-19 22:04:42 -0700

    calls on api and successfully converts response data to a Set of titles

--- src/components/backyard/textSearch/useBookSearch.js
+++ src/components/backyard/textSearch/useBookSearch.js
@@ -2,11 +2,16 @@ import { useEffect, useState } from 'react'
 import axios from "axios"
 
 export default function useBookSearch(query, pageNum) {
-
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
   //useEffect makes axios request each time the query OR
   //pageNum is updated.
   useEffect(() => {
-    
+    setLoading(true);
+    setError(false);
+
     let cancel;
     axios({
       method: 'GET',
@@ -18,9 +23,14 @@ export default function useBookSearch(query, pageNum) {
       cancelToken: new axios.CancelToken(c => cancel = c)
 
     }).then(res => {
-
+      // console.log(res.data)
+      setBooks(prevBooks => {
+        return [...new Set([...prevBooks, res.data.docs.map(b => {
+          // console.log(b.title)
+          return b.title
+        })])]
+      })
       //handles data
-      console.log(res.data)
 
     }).catch(e => {
 
