2e5b82df93f5e1452c4f1f213ce98ea2686ce2f4
DeclawedLyon <declan.e.lyons@gmail.com>
2022-04-25 17:33:42 -0700

    adds coordinates for google maps

--- src/components/backyard/Backyard.js
+++ src/components/backyard/Backyard.js
@@ -6,8 +6,22 @@ import Logo from './logo/Logo'
 import SiteLoading from './page_load/SiteLoading'
 import ObservedContainer from './observer_test/ObservedContainer'
 import GoogleMap from './map_test/GoogleMap'
+import initMap from './map_test/initMap'
 
 export default function Backyard(props) {
+  const polygonObjects = [
+    {
+      coordinates: [
+        [48.43113194793202, -123.42071126138042],
+        [48.431135507329365, -123.4205127779083],
+        [48.431893653283915, -123.42050741349016],
+        [48.431893653283915, -123.42041085396316],
+        [48.43103940351321, -123.42042694721768],
+        [48.43103940351321, -123.42070589696226],
+        [48.43113194793202, -123.42071126138042]
+      ]
+    }
+  ]
   // console.log(process.env)
   const location = {
     address: 'Colville Park',
@@ -16,10 +30,12 @@ export default function Backyard(props) {
   }
   return (
     <div id="backyard">
+      <div id='map'></div>
       {/* <SiteLoading /> */}
-      <BookSearch />
-      <GoogleMap location={location} zoomLevel={8} />
-      <ObservedContainer />
+      {/* <BookSearch /> */}
+      <GoogleMap location={location} zoomLevel={16} />
+      
+      {/* <ObservedContainer /> */}
       {/* <Logo /> */}
       <button onClick={props.transition}>Back to App</button>
     </div>
--- src/components/backyard/map_test/GoogleMap.jsx
+++ src/components/backyard/map_test/GoogleMap.jsx
@@ -1,51 +1,16 @@
-// import React, { Component } from 'react'
-// npm i --save google-map-react
-// import GoogleMapReact from 'google-map-react'
-// import { Icon } from '@iconify/react'
-// import locationIcon from '@iconify/icons-mdi/map-marker'
-
-// import Loader from 'google-maps-js-api-loader'
-// import { Loader } from '@googlemaps/js-api-loader';
-
-
-// const GoogleMap = ({text}) => {
-//   return (
-//     <div>{text}</div>
-//   )
-// };
-
-// class SimpleMap extends Component {
-//   static defaultProps = {
-//     center: {
-//       lat: 48.437391,
-//       lng: -123.419482,
-//     },
-//     xoom: 11
-//   }
-// };
-
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
-// import { Icon } from '@iconify/react'
-// import locationIcon from '@iconify/icons-mdi/map-marker'
 
-// import './map.css'
-
-// const LocationPin = ({ text }) => (
-//   <div className="pin">
-//     <Icon icon={locationIcon} className="pin-icon" />
-//     <p className="pin-text">{text}</p>
-//   </div>
-// )
+import './map.css'
 
 const GoogleMap = ({ location, zoomLevel }) => {
   const apiKey = process.env.REACT_APP_API_KEY;
   console.log(apiKey)
   return (
     <div className="map">
-      <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+      <h2 className="map-h2">Naden Route</h2>
 
-      <div className="google-map" style={{ height: '100vh', width: '100%' }}>
+      <div className="google-map" style={{ height: '40vh', width: '40vw' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: apiKey }}
           defaultCenter={location}
@@ -56,135 +21,4 @@ const GoogleMap = ({ location, zoomLevel }) => {
     </div>
   )
 }
-{/* <LocationPin
-            lat={location.lat}
-            lng={location.lng}
-            text={location.address}
-          /> */}
-
 export default GoogleMap
-
-// render() {
-//   return (
-//     // Important! Always set the container height explicitly
-//     <div style={{ height: '100vh', width: '100%' }}>
-//       <GoogleMapReact
-//         bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
-//         defaultCenter={this.props.center}
-//         defaultZoom={this.props.zoom}
-//       >
-//         <AnyReactComponent
-//           lat={59.955413}
-//           lng={30.337844}
-//           text="My Marker"
-//         />
-//       </GoogleMapReact>
-//     </div>
-//   )
-// };
-
-
-
-// export default function GoogleMap({location, zoomLevel}) {
-//   const apiKey = process.env.REACT_APP_API_KEY;
-//   console.log(apiKey)
-
-  
-//   return (
-//     <div className="map">
-//       <h2 className="map-h2">Come Visit Us At Our Campus</h2>
-
-//       <div className="google-map">
-//         <GoogleMapReact
-//           bootstrapURLKeys={{ key: apiKey }}
-//           defaultCenter={location}
-//           defaultZoom={zoomLevel}
-//         >
-//           {/* <LocationPin
-//             lat={location.lat}
-//             lng={location.lng}
-//             text={location.address}
-//           /> */}
-//         </GoogleMapReact>
-//       </div>
-//     </div>
-//   )
-// }
-
-// const LocationPin = ({ text }) => (
-  //   <div className="pin">
-  //     <Icon icon={locationIcon} className="pin-icon" />
-  //     <p className="pin-text">{text}</p>
-  //   </div>
-  // )
-
-  // const test = async static load(options: LoaderOptions, onLoadingStart?: () => void): Promise<void>
-  // const loader = new Loader({
-  //   apiKey: "",
-  //   version: "weekly",
-  //   libraries: ["places"]
-  // });
-
-  // const mapOptions = {
-  //   center: {
-  //     lat: 0,
-  //     lng: 0
-  //   },
-  //   zoom: 4
-  // };
-  // const location = {
-  //   address: 'Colville Park',
-    // lat: 48.437391,
-    // lng: -123.419482,
-    
-
-
-  // }
-
-  // loader.loadCallback(e => {
-  //   if (e) {
-  //     console.log(e);
-  //   } else {
-  //     new window.google.maps.Map(document.getElementById("map"), mapOptions);
-  //   }
-  // });
-
-  // const mapPromise = new Promise((resolve, reject) => {
-  //   let x = true;
-  //   let error = false;
-  //   if (x) {
-  //     resolve('success')
-  //   }
-  //   if(error) {
-  //     reject('fail')
-  //   }
-  // })
-
-  // mapPromise
-  //   .then((res) => console.log('does it work?', res))
-
-
-
-//   function run() {
-  
-//     // Creating Our XMLHttpRequest object 
-//     var xhr = new XMLHttpRequest();
-
-//     // Making our connection  
-//     var url = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-//     xhr.open("GET", url, true);
-
-//     // function execute after request is successful 
-//     xhr.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             console.log(this.responseText);
-//         }
-//     }
-//     // Sending our request 
-//     xhr.send();
-// }
-// run();
-  // await Loader.load({
-  //   apiKey: apiKey
-  // })
-  // console.log(apiKey)
