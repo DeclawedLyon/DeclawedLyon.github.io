5d60d95e73678613cc2e098553f1c21d6eff92d0
DeclawedLyon <declan.e.lyons@gmail.com>
2024-05-15 16:51:44 -0700

    Updates

--- src/config/development.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  database: {
-      // host: "localhost",
-      // port: 27017,
-      // username: "admin",
-      // password: "Password1",
-      apiKey: 'AIzaSyBcLPZ4XFvXcDvolaQVQ3hQMcPPWCZlWwk'
-  },
-};
